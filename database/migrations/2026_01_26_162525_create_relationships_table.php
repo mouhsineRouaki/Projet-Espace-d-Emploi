687ec339a19f3de78015dce1f25ad198b43f2b1a
Mouhsine Rouaki <houtm27@gmail.com>
2026-01-27 09:46:16 +0100

    ajouter le fichiers pour migrate table notifications et complet le migrations de table relations

--- database/migrations/2026_01_26_162525_create_relationships_table.php
+++ database/migrations/2026_01_26_162525_create_relationships_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('relationships', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('reciever_id')->constrained('users');
+            $table->enum('status',['ACCEPTED','REFUSED' , 'PENDING']);
+            $table->timestamps('date_creation')->useCurrent();
+            $table->timestamps('date_repond')->nullable();
         });
     }
 
