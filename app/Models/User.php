57669faaaf7f7e9875533d9f4dc29e3a784b99f8
Mouhsine Rouaki <houtm27@gmail.com>
2026-01-28 12:27:28 +0100

    Update User model attributes and relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-abstract class User extends Authenticatable
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
