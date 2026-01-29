50d9bfd6b2b96badbe3ceaf0c9da2e2c0d8f8229
Mouhsine Rouaki <houtm27@gmail.com>
2026-01-30 00:31:46 +0100

    Refactor relationship controller logic

--- app/Http/Controllers/RelationShipController.php
+++ app/Http/Controllers/RelationShipController.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\RelationsShip;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class RelationShipController extends Controller
 {
-    //
+    public function AjouteAmi(Request $request){
+        $reciever_id = $request->query('reciever_id');
+        RelationsShip::create(['sender_id'=>auth()->id() , 'reciever_id'=> $reciever_id]);
+
+    }
 }
