53e10c520f4f2004ec0207f2d3e62f52bd6784f6
Mouhsine Rouaki <houtm27@gmail.com>
2026-01-29 14:53:12 +0100

    Update user controller actions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,11 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function 
+    public function searchPage(Request $request)
+    {
+        $q = $request->query('q'); 
+        if(!empty($q)){
+            $users = User::where('id' ,auth()->id())->where('nom', 'ILIKE', "%{$q}%")->orWhere('prenom', 'ILIKE', "%{$q}%")->get();
+        }else{
+            $users = User::where('id' ,auth()->id())->get();
+        }
+
+        
+
+        return view('users.search', compact('users', 'q' ));
+    }
 }
