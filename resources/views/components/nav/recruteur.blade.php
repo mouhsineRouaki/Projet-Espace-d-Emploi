58a9cc414418ad90ea31cc74187c995ab0361f0b
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-05 15:41:16 +0100

    Update recruteur navigation component

--- resources/views/components/nav/recruteur.blade.php
+++ resources/views/components/nav/recruteur.blade.php
@@ -9,7 +9,7 @@
             : 'border-transparent text-gray-500 hover:text-gray-900 hover:border-indigo-300' }}">
         Dashboard
     </a>
-    <a href="{{ route('offers.index') }}"
+    <a href=""
        class="pb-3 border-b-2 {{ $active === 'dashboard'
             ? 'border-indigo-500 text-gray-900'
             : 'border-transparent text-gray-500 hover:text-gray-900 hover:border-indigo-300' }}">
